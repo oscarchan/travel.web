cb14d8c938d9676501d492165f104b2d700088cf
Oscar Chan <oscarc@surveymonkey.com>
2013-05-24 17:12:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelWeb::Application.config.secret_token = '7842e7a3128e59b25011b9d45e2b2a9f614ce37e9b829aae2da22c34e8bcfa36fc77735d33999c481b35952a749dc861518e27dde1ac529741863df4fce04ba5'
