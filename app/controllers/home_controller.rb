6feb0aadffd4b09e6f339dff192a5146fb43ba7b
Oscar Chan <oscarc@surveymonkey.com>
2013-05-30 01:15:24 -0700

    port static home page to rails view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @banner_images = ['home/homepage-1.jpg', 'home/homepage-2.jpg', 'home/homepage-3.jpg']
+    @popular_destinations = Destination.find_most_popular()
   end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,21 @@
 class Destination
-  attr_accessor :name, :banner_images
+  attr_accessor :name, :snapshot_url, :banner_image_urls
+
+  def initialize(attrs = {} )
+    attrs.each do |name, value|
+      # calling the attr accessor
+      self.send("#{name}=", value) if self.respond_to? "#{name}="
+    end
+  end
+
+
+  def self.find_most_popular
+    samples = [ { name: 'San Francisco', snapshot_url: 'destinations/san-francisco-snapshot.jpg' },
+                { name: 'Los Angeles', snapshot_url: 'destinations/los-angeles-snapshot.jpg' },
+                { name: 'San Diego', snapshot_url: 'destinations/san-francisco-snapshot.jpg' },
+                { name: 'Las Vegas', snapshot_url: 'destinations/los-angeles-snapshot.jpg' },
+                { name: 'San Luis Obispo', snapshot_url: 'destinations/san-francisco-snapshot.jpg' }]
+
+    destinations = samples.map { |sample|  Destination.new(sample)}
+  end
 end
